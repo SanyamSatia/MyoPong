43b5e45e494af5629e2a8363dc33432adb769414
7Aces <sanyam.satia@ymail.com>
2015-09-19 16:05:24 -0400

    Added socket calls on client side

--- MyPong/public/javascripts/client.js
+++ MyPong/public/javascripts/client.js
@@ -4,3 +4,19 @@ var oppSide = "left";
 $(document).mousemove(function(e){
     $("#playerPaddle").css({left:e.pageX, top:e.pageY});
 });
+
+var socket = io();
+
+function sendPaddleLoc(){
+    socket.emit('paddleLoc',$("#playerPaddle").offset());
+}
+
+socket.on('ballLoc', function(loc){
+	$("#ball").offset(loc);
+});
+
+socket.on('opponentPaddleLoc', function(loc) {
+	$("#opponentPaddleLoc").offset(loc);
+});
+
+var intervalID = setInterval(sendPaddleLoc, 100);
