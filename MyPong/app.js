7a426f462b7291796ba6d0dec1ba6aea73ad5b4a
Cristhian Ulloa <cristhian2ulloa@gmail.com>
2015-09-19 20:50:27 -0400

    started ball physics

--- MyPong/app.js
+++ MyPong/app.js
@@ -15,7 +15,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var games = require('./routes/games');
 
-var users_list = new Array(2);
+
 
 
 // view engine setup
@@ -69,37 +69,101 @@ app.get('/', function(req, res){
   res.send(__dirname + 'index.html');
 });
 
+//Define location (0, 0) to be bottom left
+//direction 0 is right, direction 180 is right, direction 90 would be up
+
+/**
+ * Ball Physics goes here
+ */
+var ball = {
+    'direction': 0, 
+    'xVel': 1, 
+    'yvel': 1, 
+    'xPos': 1,
+    'yPos': 1
+}
+
+
+//1024 * 768
+//every 35 ms update the x and y coords by 1 or 2 pixels
+//ball is 48 * 48 px
+
+/**
+ *
+ */
+
+
+var users_list = new Array(2);
+var game_ready = false;
+var player1_ready = false;
+var player2_ready = false;
+
+//TODO: method currently checks if game is ready
+//should also start the game itself
+var check_ready = function(p1, p2) {
+  if(player1_ready && player2_ready) {
+    io.emit('startBall', ball);
+  }
+}
+
+
+
+
 io.on('connection', function(socket){
   var new_user = randomstring.generate(7);
     
   //TODO: limit to 2 users max
-  if(users_list.length < 2) {
-    console.log('a user ' + '\'' + new_user + '\'' + ' connected');
-      
-    // make a random username
-    users_list.push(new_user);
-    // send the user name to the use that just logged on??
-    //TODO: send user name to client to use for identification
-    io.emit('newUser', new_user);
-    
-    
-  }
+//  if(users_list.length < 2) {
+//    console.log('a user ' + '\'' + new_user + '\'' + ' connected');
+//      
+//    // make a random username
+//    users_list.push(new_user);
+//    // send the user name to the use that just logged on??
+//    //TODO: send user name to client to use for identification
+//    io.emit('newUser', new_user);
+//    
+//  }
   
-  
+  socket.on('player1Select', function(msg){
+    console.log('Player 1 ready!');
+    io.emit('player1Taken');
+    player1_ready = true;
+    user_list[0] = socket; //TODO: what do we want to save to this list??
+    check_ready(player1_ready, player2_ready);
+  });
+    
+  socket.on('player2Select', function(msg){
+    console.log('Player 2 ready!');
+    io.emit('player2Taken');
+    player2_ready = true;
+    user_list[1] = socket; //TODO: what do we really want to save to this list?
+    check_ready(player1_ready, player2_ready);
+  });
     
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
     
-  socket.on('paddleLoc', function(msg){
-    console.log('paddleLoc: ' + msg);
-    io.emit('opponentPaddleLoc', msg);
+  //player - string that identifies the player who is ending paddle location
+  socket.on('paddleLoc', function(loc, playerID){
+    console.log('paddleLoc: ' + loc);
+    io.emit('opponentPaddleLoc', loc + ':' + playerID); //if client player id no match, it updates enemy paddle loc
   });
+    
+    
+  
     
   // will probably require the client to send the user ID for every call
   socket.on('disconnect', function(){
-    var old_user = users_list.pop(new_user);
+    var user_index = users_list.indexOf(socket);
+    user_index.pop(socket);
+    if(user_index == 0) {
+      new_user = 'Player 1';   
+    }
+    else {
+      new_user = 'Player 2';   
+    }
     console.log('user ' + '\'' + new_user + '\'' + ' disconnected');
     
   });
